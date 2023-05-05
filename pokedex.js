78f55baa024695e681b4f888ed5c49d127a0000a
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-05-05 03:04:56 -0300

    Agrega tipos y habilidades

--- pokedex.js
+++ pokedex.js
@@ -1,36 +1,79 @@
+function mostrarTipos(tipos){
+    const $tipos = document.querySelector('#tipos-container');
+    $tipos.innerHTML = '';
+
+    tipos.forEach((tipo) => {
+        const spanTipo = document.createElement('span');
+        spanTipo.className = `badge ${tipo}`;
+        spanTipo.textContent = tipo;
+        $tipos.appendChild(spanTipo);
+    })
+}
+
+function mostrarHabilidades(habilidades){
+    const $habilidades = document.querySelector('#habilidades-container');
+    $habilidades.innerHTML = '';
+
+    habilidades.forEach((habilidad) => {
+        const spanHabilidad = document.createElement('span');
+        spanHabilidad.className = 'badge-info';
+        spanHabilidad.textContent = habilidad;
+        $habilidades.appendChild(spanHabilidad);
+    })
+}
+
+function mostrarMovimientos(movimientos){
+    const $movimientos = document.querySelector('#movimientos-container');
+    $movimientos.innerHTML = '';
+
+    movimientos.forEach((movimiento) => {
+        const spanMovimiento = document.createElement('span');
+        spanMovimiento.className = 'badge-info';
+        spanMovimiento.textContent = movimiento;
+        $movimientos.appendChild(spanMovimiento);
+
+
+
+        //Hay que hacer una tabla con todos los movimientos que hay y sus versiones.
+    })
+}
+
 function mostrarPokemon(pokemon){ 
     if(document.querySelector('#ayuda')){
         document.querySelector('#ayuda').remove();
     }
-    const {name: nombre, sprites: {front_default : foto}} = pokemon
+    const {name: nombre, sprites: {front_default: foto}, 
+    types: tipos, abilities: habilidades, moves: movimientos} = pokemon;
     const imagen = document.querySelector('#pokemon-imagen');
     imagen.setAttribute('src', foto);
     imagen.setAttribute('alt', `Imagen del pokemon ${nombre}`);
     document.querySelector('#pokemon-nombre').textContent = nombre;
     document.querySelector('#pokemon-id').textContent =`#${pokemon.id} `;
+    mostrarTipos(tipos.map((items) => items.type.name));
+    mostrarHabilidades(habilidades.map((items) => items.ability.name));
+    mostrarMovimientos(movimientos.map((items) => items.move.name));
 }
 
 function cargarPokemon(nombre){
     const linkFetch = `https://pokeapi.co/api/v2/pokemon/${nombre}`
-    console.log()
     fetch(linkFetch)
     .then((res) => res.json())
     .then((r) => {
         console.log(r)
         mostrarPokemon(r);
     })
-    .catch((error) => console.log("ERROR" , error))
+    .catch((error) => console.log("ERROR", error));
 }
 
 function mostrarListaPokemones(pokemones){
     const listaPokemones = document.querySelector('#lista-pokemones');
     pokemones.forEach(function (pokemon){
         const entrada = document.createElement('a');
-        const {name: nombre, url} = pokemon
-        entrada.setAttribute('href', '#');
-        entrada.textContent = pokemon.name;
+        const {name: nombre} = pokemon;
+        //entrada.setAttribute('href', '#');
+        entrada.textContent = nombre;
         entrada.addEventListener('click', () => {
-            cargarPokemon(nombre)
+            cargarPokemon(nombre);
         })
         entrada.className = 'list-group-item list-group-item-action';
         listaPokemones.append(entrada);
@@ -49,7 +92,7 @@ function iniciar(){
             const pokemones = respuesta.results;
             mostrarTotalPokemones(totalPokemones);
             mostrarListaPokemones(pokemones);
-        })
+        }).catch((error) => console.log("ERROR", error));
 }
 
 iniciar();
