c2a744c3218f0146a659f2c0387c6dc35a24d50b
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-06-06 23:49:43 -0300

    Continua probando funciones de testing

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -3,47 +3,78 @@ const CANTIDAD_PAGINAS = 65;
 const POKEMONES_POR_PAGINA = 20;
 
 describe('Pokedex', () => {
+
+  /*beforeEach(() => {
+    cy.visit(URL);
+    cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon?offset=840&limit=20', { fixture: 'pokemonData.json' }).as('pokemonRequest');
+  })*/
+
   beforeEach(() => {
     cy.visit(URL); 
   });
 
-    /*it('Asegura que ...', () => {
-      cy.get('elemento').click();
-      cy.get('elemento').should('have.text', 'Lorem ipsum');
-    });*/
+  it('Debería obtener los datos de la API correctamente', () => {
+    cy.wait('@pokemonRequest').then((interception) => {
+      const response = interception.response.body;
+      expect(response).to.have.property('count', 20);
+      expect(response.results).to.have.length(20);
+    });
+  });
 
-   it('Asegura que se abra en la primera página', () => {
-      const linkPrimeraPagina = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`;
-      cy.window().then((ventana) => {
-        ventana.fetch(linkPrimeraPagina)
-        .then((respuesta) => respuesta.JSON())
-        .then(() => revisarFetch(linkPrimeraPagina));
+  it('Asegura que se abra en la primera página', () => {
+    const linkPrimeraPagina = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`;
+    cy.window().then((ventana) => {
+      ventana.fetch(linkPrimeraPagina)
+      .then((respuesta) => respuesta.json())
+      .then(() => revisarFetch(linkPrimeraPagina));
       });
-    });
+  });
 
-    it('Asegura que la lista tenga como máximo 20 elementos por página', () => {
-      const listaPagina = cy.get('ul-pokemones');
-      listaPagina.should('have.lengthOf.most', POKEMONES_POR_PAGINA);
-    });
+  it('Asegura que la lista tenga como máximo 20 elementos por página', () => {
+    const listaPagina = cy.get(".list-group-item list-group-item-action");
+    listaPagina.should('have.length', POKEMONES_POR_PAGINA); 
+  });
 
-    /*it('Asegura que los botones de anterior y siguiente funcionen', () => {
+  it('Asegura que los botones de anterior y siguiente funcionen', () => {
+    const botonAnterior = cy.get('#pagina-anterior');
+    const botonSiguiente = cy.get('#pagina-siguiente');
 
-    });*/
+    revisarBoton(botonAnterior, 5);
+    revisarBoton(botonSiguiente, 10);
+  });
 
   /*
   Tests para hacer:
-  Verificar que se cargue la lista (ver cómo se verifica un fetch en cypress, o esperar que se cargue para testear)
-  Verificar que la lista tenga un límite de 20 elementos por página
-  Verificar que se inicie en la página 1: `https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`;
+  Revisar que cada página tenga como máximo 20 elementos en la lista.
   Botones de anterior y siguiente
   */
 })
 
-function revisarBoton(boton, pagina, paginaNueva){
-  const elemento = cy.get(`#pagina-${pagina}`);
+function revisarElementosLista(){
+  const lista = cy.get('#lista-pokemones');
+  lista.children().should('have.length', POKEMONES_POR_PAGINA);
+}
+
+function revisarBoton(boton, pagina){
+  const linkActual = `https://pokeapi.co/api/v2/pokemon?offset=${(pagina * 20) - 20}&limit=20`;
+      cy.window().then((ventana) => {
+        ventana.fetch(linkActual)
+        .then((respuesta) => respuesta.json())
+        .then(() => {
+          if(boton === cy.get('#pagina-anterior')){
+            pagina --;
+          } else if(boton === cy.get('#pagina-siguiente')){
+            pagina ++;
+          }
+          cy.get(boton).click();
+          revisarFetch(linkActual);
+        });
+      });
+
+  /*const elemento = cy.get(`#pagina-${pagina}`);
   const lista = cy.get('#ul-pokemones');
   cy.get(boton).click();
-  //lista.child() should be paginaNueva
+  lista.children().should ('be.equal.to', paginaNueva);*/
 
   //Despues para llamar la función hago
   //revisarBoton(botonAnterior, pagina, pagina - 1);
@@ -52,6 +83,6 @@ function revisarBoton(boton, pagina, paginaNueva){
 
 function revisarFetch(linkFetch){
   cy.wait(linkFetch).then((interception) => {
-    expect(interception.response.statusCode).to.equal(200)
+    expect(interception.response.statusCode).to.equal(200);
   })
 }
